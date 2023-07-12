e86653847213ab5f4bcda5095ffc61a0ab54e188
Виолетта Цветкова <violettacvetkova@192.168.1.9>
2023-07-12 16:07:45 +0300

    init

---
+++ build/index.js
@@ -0,0 +1,5 @@
+import { analytics } from "./modules/analytics.js";
+const message = "hello node";
+console.log(message);
+analytics('Main.ts');
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { analytics } from './modules/analytics.js'
+const message = 'hello node'
+console.log(message)
+analytics('Main.ts')
