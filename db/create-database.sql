867f701905e3dac6cc87cd9e68eb242483e532fd
Виолетта Цветкова <violettacvetkova@192.168.1.9>
2023-08-13 13:29:29 +0300

    fix: fix docker image

--- db/create-database.sql
+++
@@ -1,5 +0,0 @@
-DO $$ BEGIN
-    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'deals') THEN
-        CREATE DATABASE deals;
-    END IF;
-END $$;
--- db/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-psql -v ON_ERROR_STOP=1 --username "$DB_USER" --dbname "$DB_NAME" <<-EOSQL
-   \i /docker-entrypoint-initdb.d/create-database.sql
-   \i /docker-entrypoint-initdb.d/dump.sql
-EOSQL
